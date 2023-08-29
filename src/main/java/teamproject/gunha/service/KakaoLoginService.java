59902de3d3dbd3035efe228de9b16817cce6c60b
limsky96 <moodforcozy@gmail.com>
2023-08-29 16:49:23 +0900

    MOVIE DB insert 노가다

--- src/main/java/teamproject/gunha/service/KakaoLoginService.java
+++
@@ -1,9 +0,0 @@
-package teamproject.gunha.service;
-
-import teamproject.gunha.security.vo.OAuthTokenKakao;
-
-public interface KakaoLoginService {
-  public OAuthTokenKakao getOAuthToken(String code);
-  public String getKakaoProfile(OAuthTokenKakao oAuthToken) ;
-  public boolean logoutKakao(OAuthTokenKakao oAuthToken);
-}
