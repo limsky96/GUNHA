91b01499b95d3cd9cb4f8bfa62c92993a550371b
limsky96 <moodforcozy@gmail.com>
2023-08-31 16:21:35 +0900

    영화 리스트 CRUD

--- src/main/java/teamproject/gunha/controller/MovieController.java
+++ src/main/java/teamproject/gunha/controller/MovieController.java
@@ -3,16 +3,21 @@ package teamproject.gunha.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import teamproject.gunha.mapper.MovieMapper;
 import teamproject.gunha.vo.MovieVO;
 
-@RestController // 엔드포인트 URL
+@Controller
 public class MovieController {
 
     // MovieRepository : 데이터베이스와 상호작용하여 영화 정보를 가져오는 역할
@@ -20,7 +25,48 @@ public class MovieController {
     private MovieMapper movieMapper; // MovieRepository 인터페이스의 구현체를 주입받음
 
     @GetMapping("/api/movies") // api/movies (GET) 메소드 호출
+    @ResponseBody
     public List<MovieVO> getMovies() { // 영화 목록 가져오는 메소드
         return movieMapper.findMovieList(); // DB에 저장된 모든 영화 정보를 가져온 후, 리스트로 변환
+    }
+
+    // CRUD
+    @PostMapping
+    public ResponseEntity<MovieVO> createMovie(@RequestBody MovieVO newMovie) {
+        movieMapper.save(newMovie);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newMovie);
+    }
+
+    // 단일 영화 정보 조회 기능
+    @GetMapping("/{id}")
+    public ResponseEntity<MovieVO> getMovie(@PathVariable Long id) {
+        MovieVO movie = movieMapper.findById(id);
+        if (movie != null) {
+            return ResponseEntity.ok(movie);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MovieVO> updateMovie(@PathVariable Long id, @RequestBody MovieVO updatedMovie) {
+        MovieVO movie = movieMapper.findById(id);
+        if (movie != null) {
+            movie.updateFrom(updatedMovie);
+            movieMapper.update(movie);
+            return ResponseEntity.ok(movie);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMovie(@PathVariable Long id) {
+        if (movieMapper.existsById(id)) {
+            movieMapper.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- src/main/java/teamproject/gunha/vo/MovieVO.java
+++ src/main/java/teamproject/gunha/vo/MovieVO.java
@@ -5,10 +5,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import java.util.Date;
 
 @Data
@@ -23,4 +19,7 @@ public class MovieVO {
     private String movieCast;
     private Date movieReleaseDate;
     private boolean movieFavorite;
-}
+    
+    public void updateFrom(MovieVO updatedMovie) {
+        }
+    }
