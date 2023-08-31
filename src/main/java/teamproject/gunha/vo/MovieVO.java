e5e116cb44ecc8d7a20be4af3d488d323f481d5f
HOYA\jihyo <ggouma34@gmail.com>
2023-08-31 15:09:37 +0900

    슬라이드 개별적용, 무한스크롤로 인해 새로 생성되는 부분도 적용 완료

--- src/main/java/teamproject/gunha/vo/MovieVO.java
+++ src/main/java/teamproject/gunha/vo/MovieVO.java
@@ -5,10 +5,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import java.util.Date;
 
 @Data
