0172696c1b7907ccfe909acbc0d12b1934d591dc
geoni4 <seralove4@gmail.com>
2023-09-06 00:50:14 +0900

    토큰 얻고 일회성 결제(+빌링키 발급)까지 했음

--- src/main/java/teamproject/gunha/mapper/OrderMapper.java
+++ src/main/java/teamproject/gunha/mapper/OrderMapper.java
@@ -2,6 +2,7 @@ package teamproject.gunha.mapper;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import teamproject.gunha.vo.OrderVO;
 
@@ -9,7 +10,10 @@ import teamproject.gunha.vo.OrderVO;
 public interface OrderMapper {
   
   @Insert("insert into netflix_order(order_id, order_member_id, order_member_card_number, order_start_date)"
-        + " values(select max(*)+1 from netflix_order, #{memberId}, #{cardNumber}, #{startDate})")
+        + " values(select nvl(max(order_id),0)+1 from netflix_order, #{memberId}, #{cardNumber}, #{startDate})")
   public int insertOrder(OrderVO orderVO);
   
+  @Select("select nvl(max(order_id),0)+1 from netflix_order")
+  public int selectNextOrderId();
+
 }
--- src/main/java/teamproject/gunha/service/OrderServiceImpl.java
+++ src/main/java/teamproject/gunha/service/OrderServiceImpl.java
@@ -10,17 +10,24 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import teamproject.gunha.mapper.OrderMapper;
 import teamproject.gunha.vo.PortOneVO;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
+
+  private final OrderMapper orderMapper;
 
   private String REST_API_KEY = "3080354631487168";
   private String REST_API_SECRET = "dOMgxkwCkBv4rlbLPKcAMud3VRe0XNgIKfnCUEGuz83pVvd1aFhCGUQX9fZeD5VfDrSfwUczdZcr2Daw";
   private String PG_MID = "iamport01m";
   private String PG = "nice_v2." + PG_MID;
+  private String mUid = "project_netflix_muid_";
+  private String cUid = "_cuid_order_";
 
   @Override
   public Map<String, Object> getAccessToken() {
@@ -51,53 +58,59 @@ public class OrderServiceImpl implements OrderService {
 
   @Override
   public Map<String, Object> issueBilling(PortOneVO portOneVO, String accessToken) {
+    String orderId = String.format("%06d", orderMapper.selectNextOrderId());
+    String merchantUid = mUid + orderId;
+    String customrUid = portOneVO.getUserId()+"_cuid_order_" + orderId;
+    portOneVO.setMerchantUid(merchantUid);
+    portOneVO.setCustomerUid(customrUid);
+    log.info(portOneVO.toString());
+    
     RestTemplate rt = new RestTemplate();
-
+    
+    
     // HttpHeader 오브젝트 생성
     HttpHeaders requestHeader = new HttpHeaders();
     requestHeader.add("Authorization", accessToken);
-
-    log.info(portOneVO.toString());
+    
     // Http Body 오브젝트 생성
-    Map<String, Object> requestBody = new HashMap<>();
     String url = "https://api.iamport.kr/subscribe/payments/onetime";
-    String mUid = "netofuri_muid_000008";
-    String cUid = "customer_uid_000008";
+
+    Map<String, Object> requestBody = new HashMap<>();
     // card_number, // 카드 번호
     // expiry, // 카드 유효기간
     // birth, // 생년월일
     // pwd_2digit, // 카드 비밀번호 앞 두자리
     // pg: YOUR_PG_HERE, // 빌링키 발급에 사용할 PG
-    requestBody.put("merchant_uid", mUid);
-    requestBody.put("customer_uid", cUid);
+    requestBody.put("merchant_uid", portOneVO.getMerchantUid());
+    requestBody.put("customer_uid", portOneVO.getCustomerUid());
     requestBody.put("card_number", portOneVO.getCardNumber());
     requestBody.put("expiry", portOneVO.getExpiry());
     requestBody.put("birth", portOneVO.getBirth());
     requestBody.put("pwd_2digit", portOneVO.getPwd2digit());
     requestBody.put("pg", PG);
     requestBody.put("amount", 100);
-    requestBody.put("name", "구독");
+    requestBody.put("name", "베이직 구독");
     log.info(requestBody.toString());
     // HttpHeader와 HttpBody를 하나의 오브젝트에 담기
     HttpEntity<Map<String, Object>> billingCodeRequest = new HttpEntity<>(requestBody, requestHeader);
 
     // log.info(accessTokenRequest.toString());
 
-    ResponseEntity<Map> billingCodeResponse = rt.exchange(url, HttpMethod.POST,
-        billingCodeRequest, Map.class);
+    // ResponseEntity<Map> billingCodeResponse = rt.exchange(url, HttpMethod.POST,
+    //     billingCodeRequest, Map.class);
 
-    log.info(billingCodeResponse.toString());
+    // log.info(billingCodeResponse.toString());
 
     Map<String, Object> resp = new HashMap<>();
-
-    int code = (int) billingCodeResponse.getBody().get("code");
-    if (code == 0) { // 빌링키 발급 성공
-      resp.put("status", "success");
-      resp.put("message", "Billing has successfully issued");
-    } else { // 빌링키 발급 실패
-      resp.put("status", "failed");
-      resp.put("message", (String) billingCodeResponse.getBody().get("message"));
-    }
+    resp.put("status", "failed");
+    // int code = (int) billingCodeResponse.getBody().get("code");
+    // if (code == 0) { // 빌링키 발급 성공
+    //   resp.put("status", "success");
+    //   resp.put("message", "Billing has successfully issued");
+    // } else { // 빌링키 발급 실패
+    //   resp.put("status", "failed");
+    //   resp.put("message", (String) billingCodeResponse.getBody().get("message"));
+    // }
     return resp;
   }
 
